a0d9072d3cb59a9fc85c03e49610d6c76eabb9fd
Artem Pogosov <artem.pogosov@precisionformedicine.com>
2025-03-26 22:43:33 +0200

    NvChad: add Princess Mononoke header

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,46 +1,76 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
-require('custom.telescope').setup()
-require("telescope").load_extension "file_browser"
+require("custom.telescope").setup()
+require "custom.autocmds"
 
 local M = {}
-local git_modules = require ("custom.git").modules
-
--- Change a directory when opening a file
-vim.api.nvim_create_autocmd("BufEnter", {
-  callback = function()
-    vim.cmd("silent! cd %:p:h")
-  end
-})
+local git_modules = require("custom.git").modules
 
 M.base46 = {
   theme = "gruvbox",
   theme_toggle = { "gruvbox", "gruvbox_light" },
 }
 
-M.nvdash = { load_on_startup = true }
+M.nvdash = {
+  load_on_startup = true,
+  header = {
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣦⡀⢰⠒⠒⠦⣀⠴⠒⠒⡆⢀⣴⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⡉⠁⠀⠀⠀⠀⠀⠈⢉⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⢀⠔⢺⣿⣿⣿⠿⠛⠛⠉⠀⠀⠀⠀⠀⠀⠀⠉⠛⠛⠿⢿⣿⣿⡟⠢⡄⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⢀⡜⠁⠀⠀⠛⠁⠀⠀⣠⣴⣶⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⡀⠀⠈⠙⠀⠀⠈⠣⡀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⢀⡎⠀⠀⠀⠀⠀⣀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⢹⡀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠘⡲⠋⠀⠀⠀⣠⣤⣤⣤⣤⡙⠿⣿⣿⣿⣿⣿⣿⣿⡿⢋⣤⣤⣤⣤⣤⠀⠀⠀⠙⢶⠃⠀⠀⠀⠀",
+    "⠀⠀⠀⢠⠞⠀⠀⠀⠀⣼⣿⡿⠟⠛⠿⣿⣧⡘⢿⣿⣿⣿⣿⢋⣴⣿⠿⠛⠻⢿⣿⣧⠀⠀⠀⠀⠱⡄⠀⠀⠀",
+    "⠀⠀⢀⠏⠀⠀⠀⠀⢸⡿⠁⣠⣴⣶⣤⡈⠻⣷⡈⣿⣿⣿⠁⣾⠟⢁⣤⣶⣦⣄⠈⢿⡇⠀⠀⠀⠀⠹⡀⠀⠀",
+    "⠀⠀⣾⠀⠀⠀⠀⠀⣾⡇⢸⣿⣿⣿⣿⣧⠀⣿⣧⠸⣿⠇⣼⣿⠀⣼⣿⣿⣿⣿⡇⢸⣿⠀⠀⠀⠀⠀⢷⠀⠀",
+    "⠀⠀⡇⠀⡀⠀⠀⠀⣿⣧⠈⠿⣿⣿⡿⠃⣰⣿⣿⣦⣤⣴⣿⣿⣆⠘⢿⣿⣿⠿⠁⣼⣿⠀⠀⠀⢀⠀⢸⠀⠀",
+    "⠀⠀⠧⠞⣇⠀⠀⠀⢻⣿⣷⣦⣤⣤⣤⣾⣿⡿⠛⠛⠛⠛⠛⠿⣿⣷⣦⣤⣤⣴⣾⣿⡟⠀⠀⠀⢸⠓⠼⠀⠀",
+    "⠀⠀⠀⠀⡏⠀⠀⠀⠈⠉⠛⠛⠛⠛⠛⣋⣤⣶⣿⠿⠿⠿⣿⣶⣤⣉⠛⠛⠛⠛⠛⠉⠁⠀⠀⠀⢸⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⢣⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⡟⢁⣴⣶⣦⡈⢻⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⡸⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠘⡆⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⡇⣾⣿⣿⣿⣿⢈⣿⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⢰⠃⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠘⢆⠀⣄⠀⠀⠀⠉⠻⣿⣿⣿⣷⣌⠛⠛⠛⣡⣾⣿⣿⣿⠟⠋⠀⠀⠀⣠⠀⣰⠃⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠈⠓⠛⠑⢤⡀⠀⠀⠀⠈⠙⠛⠛⠻⠿⠿⠛⠛⠋⠉⠀⠀⠀⢀⡤⠊⠙⠚⠁⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠢⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠔⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠒⠤⣀⡀⠀⠀⠀⠀⣀⡤⠖⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀ To see with eyes unclouded by hate.⠀⠀ ",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+  },
+
+  buttons = {
+    { txt = "  Find File", keys = "SPC f f", cmd = "Telescope find_files" },
+    { txt = "  Recent Files", keys = "SPC f o", cmd = "Telescope oldfiles" },
+    { txt = "󰈭  Find Word", keys = "SPC f w", cmd = "Telescope live_grep" },
+    -- TODO: complete list
+  },
+}
 
 M.ui = {
   statusline = {
-    theme = "vscode_colored",
+    theme = "default",
     separator_style = "default",
-    order = { "f", "file_status", "%=", "lsp_msg", "%=", "lsp", "git" },
+    order = { "f", "file_status", "%=", "diagnostics", "lsp_msg", "space", "git" },
     modules = {
       f = " %F",
       git = git_modules.statusline.git_custom,
       file_status = function()
         return vim.bo.modified and " [+]" or ""
       end,
-    }
+      space = " ",
+    },
   },
-  -- lazyload it when there are 1+ buffers
+
   tabufline = {
     enabled = true,
     lazyload = true,
-    order = { "treeOffset", "buffers", "tabs", "btns" },
+    order = { "treeOffset", "buffers", "tabs" },
     modules = nil,
     bufwidth = 21,
   },
