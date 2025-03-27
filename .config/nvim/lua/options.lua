37cca2eeb585126e712afccda7dd8d64bc12b2c1
Artem Pogosov <artem.pogosov@precisionformedicine.com>
2025-03-27 17:38:08 +0200

    NvChad: change vim-sneak colors

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -10,3 +10,6 @@ o.relativenumber = false
 opt.ignorecase = false
 opt.whichwrap = "<,>,[,],b,s"
 opt.termguicolors = true
+
+vim.api.nvim_set_hl(0, "Sneak", { fg = "#FBF1C7", bg = "#928374" })
+vim.api.nvim_set_hl(0, "SneakCurrent", { fg = "#D79921", bg = "NONE", bold = true })
