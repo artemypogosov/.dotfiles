126402e4571fe7d31c51639d8878a7d8a07359e0
Artem Pogosov <artem.pogosov@precisionformedicine.com>
2025-03-27 17:39:28 +0200

    NvChad: fix 'lclose' after killing buffer

--- .config/nvim/lua/custom/autocmds.lua
+++ .config/nvim/lua/custom/autocmds.lua
@@ -9,7 +9,9 @@ vim.api.nvim_create_autocmd("DiagnosticChanged", {
   callback = function()
     local diagnostics = vim.diagnostic.get(0) -- Get diagnostics for the current buffer
     if #diagnostics == 0 then
-      vim.cmd("lclose") -- Close the location list if no errors
+      vim.schedule(function()
+        vim.cmd("lclose") -- Close location list safely
+      end)
     else
       vim.diagnostic.setloclist({ open = false }) -- Update loclist but don't reopen it
     end
