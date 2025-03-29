806f8eb783a4c5877640749814376c0b2c2477a1
Artem Pogosov <artem.pogosov@precisionformedicine.com>
2025-03-29 11:40:07 +0200

    DOOM: prepare for big config update

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,28 +1,21 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-
 (require 'battery)
 (require 'lsp)
 (require 'lsp-haskell)
-;;
-;;(setq max-lisp-eval-depth 10000)
-;;
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
 
+;; Place your private configuration here.
+;; Remember, you do not need to run 'doom sync' after modifying this file.
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name    "Artemy Pogosov"
+(setq user-full-name    "Artem Pogosov"
       user-mail-address "artemypogosov@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones: doom-font, doom-variable-pitch-font, doom-big-font
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
+;;;;;;;;;;;;;
+;;; FONTS ;;;
+;;;;;;;;;;;;;
 
-;; >>> FONTS
 ;; ‘doom-font’ – standard monospace font that is used for most things in Emacs.
 ;; ‘doom-variable-pitch-font’ – variable font which is useful in some Emacs plugins.
 ;; ‘doom-big-font’ – used in doom-big-font-mode; useful for presentations.
@@ -32,6 +25,10 @@
 (setq doom-font (font-spec :family "Source Code Pro" :size  15)
       doom-variable-pitch-font (font-spec :family "Ubuntu" :size 15)
       doom-big-font (font-spec :family "Source Code Pro" :size 20))
+
+;;;;;;;;;;;;;;
+;;; THEMES ;;;
+;;;;;;;;;;;;;;
 
 (after! doom-themes
   (setq doom-themes-enable-bold t
@@ -43,22 +40,20 @@
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
+;; `load-theme' function.
 (setq doom-theme 'doom-gruvbox)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Org/")
 (setq org-hide-emphasis-markers t)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
-
 ;; PROJECTILE
-(setq projectile-project-search-path '("~/IdeaProjects"))
-
+(setq projectile-project-search-path '("~/Projects"))
 
 ;; OTHER
 (setq undo-limit 80000000               ; Raise undo-limit to 80Mb
@@ -67,12 +62,8 @@
       truncate-string-ellipsis "…"      ; Unicode ellispis are nicer than "...", and also save /precious/ space
       password-cache-expiry nil)        ; I can trust my computers ... can't I?
 
-(display-time-mode 1)
+(display-time-mode 0)
 
-; On laptops it's nice to know how much power you have
-;(when (and (funcall battery-status-function)
-;           (not (string-match-p "N/A" (battery-format "%B"(funcall battery-status-function)))))
-;  (display-battery-mode 1))
 
 ;; (global-subword-mode 1)
 
@@ -88,12 +79,60 @@
     (org-map-entries (lambda () (org-todo state)) nil scope)))
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; MY FUNCTIONS AND BINDINGS ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Examples
+;;(map! :leader "c z" #'kill-buffer-and-window)
+;;(map! :nv "s" (cmd! (evil-ex "%s//g")))
+(setq evil-ex-substitute-case 'sensitive)
+
+(defun substitute-and-restore (mode)
+ (interactive)
+  (save-excursion
+    (let ((original-pos (point))
+          (expression (cond
+                        ((string= mode "with-confirmation") "%s##gc")
+                        ((string= mode "no-confirmation") "%s##g")
+                        (t "default"))))
+      ;; Perform the substitution
+      (minibuffer-with-setup-hook
+          (lambda () (backward-char (if (string= mode "with-confirmation") 3 2)))
+        (evil-ex expression))
+      (goto-char original-pos))))
+
+
+(defun substitute-and-restore-dispatch ()
+  "Dispatch `substitute-and-restore` with the correct mode based on the key pressed."
+  (interactive)
+  ;; Get the pressed key
+  (let* ((key (key-description (this-command-keys)))
+         (mode (cond
+                ((string= key "f") "with-confirmation")
+                ((string= key "F") "no-confirmation")
+                (t "default"))))
+    (substitute-and-restore mode)))
+
+
+(evil-define-key 'normal 'global (kbd "f") 'substitute-and-restore-dispatch)
+(evil-define-key 'normal 'global (kbd "F") 'substitute-and-restore-dispatch)
+
+
+(setq evil-snipe-scope 'buffer)
+
+
+(custom-set-faces
+ '(org-level-1 ((t (:inherit outline-1 :height 1.05)))))
+
+;; -------------------------------------------------------------------
+;;
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
-;; - `load!' for loading external *.el files relative to this one
+;; - `Load!' for loading external *.el files relative to this one
 ;; - `use-package!' for configuring packages
 ;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
+;; - `Add-load-path!' for adding directories to the `load-path', relative to
 ;;   this file. Emacs searches the `load-path' when you load packages with
 ;;   `require' or `use-package'.
 ;; - `map!' for binding new keys
--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,46 +3,22 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#fbf1c7" "#9d0006" "#79740e" "#b57614" "#076678" "#b16286" "#427b58" "#282828"])
  '(custom-safe-themes
-   '("c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" "7eea50883f10e5c6ad6f81e153c640b3a288cd8dc1d26e4696f7d40f754cc703" "5784d048e5a985627520beb8a101561b502a191b52fa401139f4dd20acb07607" default))
- '(exwm-floating-border-color "#d3c5a0")
- '(fci-rule-color "#504945")
- '(highlight-tail-colors ((("#eee4b4" "#f3f3c1") . 0) (("#e8e5bb" "#eff3cf") . 20)))
- '(jdee-db-active-breakpoint-face-colors (cons "#f0f0f0" "#a89984"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#f0f0f0" "#79740e"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#f0f0f0" "#928374"))
- '(objed-cursor-color "#9d0006")
- '(pdf-view-midnight-colors (cons "#282828" "#fbf1c7"))
- '(rustic-ansi-faces
-   ["#fbf1c7" "#9d0006" "#79740e" "#b57614" "#076678" "#b16286" "#427b58" "#282828"])
- '(vc-annotate-background "#fbf1c7")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#79740e")
-    (cons 40 "#8d7410")
-    (cons 60 "#a17512")
-    (cons 80 "#b57614")
-    (cons 100 "#b3620e")
-    (cons 120 "#b14e08")
-    (cons 140 "#af3a03")
-    (cons 160 "#af472e")
-    (cons 180 "#b0545a")
-    (cons 200 "#b16286")
-    (cons 220 "#aa415b")
-    (cons 240 "#a32030")
-    (cons 260 "#9d0006")
-    (cons 280 "#9a2021")
-    (cons 300 "#97413c")
-    (cons 320 "#946258")
-    (cons 340 "#504945")
-    (cons 360 "#504945")))
- '(vc-annotate-very-old-color nil))
+   '("456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641"
+     "e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0"
+     "93011fe35859772a6766df8a4be817add8bfe105246173206478a0706f88b33d"
+     "2078837f21ac3b0cc84167306fa1058e3199bbd12b6d5b56e3777a4125ff6851"
+     "dd4582661a1c6b865a33b89312c97a13a3885dc95992e2e5fc57456b4c545176"
+     "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8"
+     "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700"
+     "d481904809c509641a1a1f1b1eb80b94c58c210145effc2631c1a7f2e4a2fdf4"
+     "34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec"
+     "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69"
+     "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1"
+     "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-comment-face ((t (:slant italic))))
- '(font-lock-keyword-face ((t (:slant italic)))))
+ )
