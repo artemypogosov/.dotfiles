fc4ec62fce16de01050759dde09840acf750be65
Artem Pogosov <artem.pogosov@precisionformedicine.com>
2025-03-30 00:13:52 +0200

    DOOM: update custom.el

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641"
+   '("691d671429fa6c6d73098fc6ff05d4a14a323ea0a18787daeb93fde0e48ab18b"
+     "456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641"
      "e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0"
      "93011fe35859772a6766df8a4be817add8bfe105246173206478a0706f88b33d"
      "2078837f21ac3b0cc84167306fa1058e3199bbd12b6d5b56e3777a4125ff6851"
@@ -21,4 +22,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-level-1 ((t (:inherit outline-1 :height 1.05)))))
