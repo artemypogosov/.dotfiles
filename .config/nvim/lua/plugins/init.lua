8743afe9be60807982f3982ae0261e369ce280a9
Artem Pogosov <artemypogosov@gmail.com>
2025-07-28 11:52:55 +0300

    NEOVIM: Remove NvChad config files.

--- .config/nvim/lua/plugins/init.lua
+++
@@ -1,8 +0,0 @@
-return {
-  { import = "plugins.core" },
-  { import = "plugins.telescope" },
-  { import = "plugins.workspaces" },
-  { import = "plugins.lsp" },
-  { import = "plugins.ui" },
-  { import = "plugins.utils" },
-}
