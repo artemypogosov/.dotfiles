74cf55a44d67885deadb5b01c4af63357514cfe3
Artem Pogosov <artemypogosov@gmail.com>
2025-11-28 16:34:02 +0200

    NEOGIT: lsp and formatters update.
    Modify formatters and lsp servers.
    No longer need to install formatters via Mason.
    Use 'pacman' instead. Lsp servers are still better
    to install using Mason.

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,15 +1,46 @@
 return {
+  -- Lightweight yet powerful formatter plugin for Neovim
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
   opts = {
+    -- You can customize some of the format options for the filetype (:help conform.format)
+    -- Conform will run the first available formatter
     formatters_by_ft = {
+      -- Core
       lua = { "stylua" },
-      -- Conform will run multiple formatters sequentially
-      -- You can customize some of the format options for the filetype (:help conform.format)
-      -- Conform will run the first available formatter
-      javascript = { "prettierd", "prettier", stop_after_first = true },
-      typescript = { "prettierd", "prettier", stop_after_first = true },
 
+      -- Markup & Templates
+      html = { "biome", "prettierd", "prettier", stop_after_first = true },
+      markdown = { "prettierd", "prettier" },
+      mdx = { "biome", "prettierd", "prettier", stop_after_first = true },
+
+      -- Styling
+      css = { "biome", "prettierd", "prettier", stop_after_first = true },
+      scss = { "biome", "prettierd", "prettier", stop_after_first = true },
+      less = { "biome", "prettierd", "prettier", stop_after_first = true },
+      postcss = { "biome", "prettierd", "prettier", stop_after_first = true },
+
+      -- JavaScript / TypeScript
+      javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
+      javascriptreact = { "biome", "prettierd", "prettier", stop_after_first = true },
+      typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "biome", "prettierd", "prettier", stop_after_first = true },
+
+      -- Web UI Frameworks
+      vue = { "biome", "prettierd", "prettier", stop_after_first = true },
+      svelte = { "biome", "prettierd", "prettier", stop_after_first = true },
+      astro = { "biome", "prettierd", "prettier", stop_after_first = true },
+      -- Data formats
+      json = { "biome", "prettierd", "prettier", stop_after_first = true },
+      jsonc = { "biome", "prettierd", "prettier", stop_after_first = true },
+      yaml = { "prettierd", "prettier" },
+      yml = { "prettierd", "prettier" },
+
+      -- Node / tooling configs
+      graphql = { "prettierd", "prettier" },
+      prisma = { "prettierd", "prettier" },
+
+      -- Shells
       sh = { "shfmt", "beautysh", stop_after_first = true },
       bash = { "shfmt", "beautysh", stop_after_first = true },
       zsh = { "shfmt", "beautysh", stop_after_first = true },
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -183,24 +183,27 @@ return {
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      ts_ls = {},
-      bashls = {},
-      -- clangd = {},
-      -- gopls = {},
-      -- pyright = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`ts_ls`) will work just fine
-      -- ts_ls = {},
-      --
+      -- Core Web Languages
+      ts_ls = {}, -- TypeScript / JavaScript
+      html = {}, -- HTML
+      cssls = {}, -- CSS / SCSS / LESS
+      jsonls = {}, -- JSON
+      yamlls = {}, -- YAML
+
+      -- Framework ecosystems
+      svelte = {}, -- Svelte
+      vue_ls = {}, -- Vue
+      astro = {}, -- Astro
+
+      -- Styling systems
+      tailwindcss = {}, -- Tailwind CSS
+
+      -- Node & tooling
+      bashls = {}, -- Shell scripts
+      dockerls = {}, -- Docker
+      prismals = {}, -- Prisma schema
 
       lua_ls = {
-        -- cmd = { ... },
-        -- filetypes = { ... },
-        -- capabilities = {},
         settings = {
           Lua = {
             workspace = { checkThirdParty = false },
@@ -228,13 +231,9 @@ return {
     -- for you, so that they are available from within Neovim.
 
     local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      "stylua", -- Used to format Lua code
-      "prettierd", -- JS formatter daemon
-      "prettier", -- JS formatter
-      "shfmt", -- sh, bash, zsh formatter
-      "beautysh",
-    })
+    -- Only LSP servers here. You can add formatters inside {}, but it is better to handle
+    -- formatters binaries using 'Pacman' package manager
+    vim.list_extend(ensure_installed, {})
     require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
     require("mason-lspconfig").setup({
