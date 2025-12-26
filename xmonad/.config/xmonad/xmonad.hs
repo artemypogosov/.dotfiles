c5936e7e31de4f20378ff56b3da5ab8ec8ecea1a
Artem Pogosov <artemypogosov@gmail.com>
2025-12-27 00:52:51 +0200

    XMONAD: reduce mySpacing

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -140,7 +140,7 @@ myTall = renamed [Replace "tall"]
   $ windowNavigation
  -- $ subLayout [] (smartBorders Simplest)
   $ limitWindows 5
-  $ mySpacing 15
+  $ mySpacing 10
   $ ResizableTall 1 (3/100) (1/2) []
 
 myMirror = renamed [Replace "mirror tall"]
@@ -148,7 +148,7 @@ myMirror = renamed [Replace "mirror tall"]
   $ Mirror myTall
 
 myGrid = renamed [Replace "grid"]
-  $ mySpacing 5
+  $ mySpacing 10
   $ limitWindows 4
   Grid
 
