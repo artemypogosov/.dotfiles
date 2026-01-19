0b6c490333d1bbb4153b638f7b7d20edfbc7cdaf
Artem Pogosov <artemypogosov@gmail.com>
2026-01-19 22:58:50 +0200

    NEOVIM: some avante changes

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -1,3 +1,10 @@
+-- Use your Neovim like using Cursor AI IDE!
+
+-- Some keys to remember:
+-- Ctrl + n/p --> next/prev prompt
+-- A/a --> apply all/cursor
+-- r/e --> retry/edit user previous request
+-- @/d --> add/remove file
 return {
   "yetone/avante.nvim",
   build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
@@ -7,14 +14,13 @@ return {
   ---@module 'avante'
   ---@type avante.Config
   opts = {
-    -- this file can contain specific instructions for your project
     mode = "legacy",
+    -- This file can contain specific instructions for your project
     instructions_file = "avante.md",
     provider = "gemini", -- openai, gemini
     providers = {
       openai = {
         endpoint = "https://api.openai.com/v1",
-        -- gpt-4o (Higher quality, slower, more expensive)
         model = "gpt-4o-mini",
         timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
         context_window = 128000, -- Number of tokens to send to the model for context
@@ -29,16 +35,6 @@ return {
         temperature = 0,
         max_tokens = 8192,
       },
-      -- claude = {
-      -- endpoint = "https://api.anthropic.com",
-      -- model = "claude-sonnet-4-5-20250929",
-      -- timeout = 30000,
-      -- context_window = 200000,
-      -- extra_request_body = {
-      -- temperature = 0.75,
-      -- max_tokens = 20480, -- 4096
-      -- },
-      -- },
     },
     -- Add here you custom shortcuts which will expand in a prompt
     shortcuts = {
@@ -59,30 +55,13 @@ return {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
-      "nvim-mini/mini.pick", -- for file_selector provider mini.pick
-      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      -- "nvim-mini/mini.pick", -- for file_selector provider mini.pick
+      -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      -- "ibhagwan/fzf-lua", -- for file_selector provider fzf
       "stevearc/dressing.nvim", -- for input provider dressing
       "folke/snacks.nvim", -- for input provider snacks
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
       "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
       {
         -- Make sure to set this up properly if you have lazy=true
         "MeanderingProgrammer/render-markdown.nvim",
@@ -94,29 +73,3 @@ return {
     },
   },
 }
-
------------
--- KEYS --
------------
-
--- C-n/p - next/prev prompt
-
--- diff = {
---   ours = "co",
---   theirs = "ct",
---   all_theirs = "ca",
---   cursor = "cc",
---   next = "]x",
---   prev = "[x",
--- },
--- jump = {
---   next = "]]",
---   prev = "[[",
--- },
--- submit = {
---   normal = "<CR>",
---   insert = "<C-s>",
--- },
--- sidebar = {
---   add_file = "@",
--- }
